f27cd3ccaa92b8a3e34bd2950a1154176b67e843
CHOYSEN <choysen@qq.com>
2021-02-20 01:03:15 +0800

    feat: add leetcode#9

--- leetcode/9.js
+++ leetcode/9.js
@@ -1,7 +1,22 @@
 /**
- * @param {number} x
- * @return {boolean}
+ * @param {string[]} strs
+ * @return {string}
  */
-var isPalindrome = function (x) {
-  return Number(x.toString().split('').reverse().join('')) === x
+var longestCommonPrefix = function (strs) {
+  if (!strs.length) {
+    return ''
+  }
+
+  let prefix = ''
+  const first = strs[0]
+  for (let i = 0; i < first.length; i++) {
+    const letter = first[i]
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== letter) {
+        return prefix
+      }
+    }
+    prefix += letter
+  }
+  return prefix
 }
