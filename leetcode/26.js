be631d73cc108f43c3978dea9aee84a2f8d871fb
CHOYSEN <choysen@qq.com>
2021-03-07 18:14:10 +0800

    feat: add leetcode#26

---
+++ leetcode/26.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let left = 0, right = 1
+  while (right < nums.length) {
+    if (nums[left] !== nums[right]) {
+      left++
+      nums[left] = nums[right]
+    }
+    right++
+  }
+  return left + 1
+};
