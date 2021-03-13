d3212b2f3cc8bfa27bf00bbd13d583dc1ba4ddb9
CHOYSEN <choysen@qq.com>
2021-03-13 22:01:33 +0800

    chore: add comment for leetcode#7

--- leetcode/7.js
+++ leetcode/7.js
@@ -3,7 +3,9 @@
  * @return {number}
  */
 var reverse = function (x) {
+  // 记录原数字的正负号
   const signal = x < 0 ? -1 : 1
+  // 转为正数方便分割后反转
   x = Math.abs(x)
 
   x = Number(x.toString().split('').reverse().join('')) * signal
