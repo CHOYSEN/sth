2a711437b370b4cda7fed1c447c6dd1b48458b13
CHOYSEN <choysen@qq.com>
2021-03-13 22:04:27 +0800

    Revert "feat: add leetcode#9"
    This reverts commit f27cd3ccaa92b8a3e34bd2950a1154176b67e843.

--- leetcode/9.js
+++ leetcode/9.js
@@ -1,22 +1,7 @@
 /**
- * @param {string[]} strs
- * @return {string}
+ * @param {number} x
+ * @return {boolean}
  */
-var longestCommonPrefix = function (strs) {
-  if (!strs.length) {
-    return ''
-  }
-
-  let prefix = ''
-  const first = strs[0]
-  for (let i = 0; i < first.length; i++) {
-    const letter = first[i]
-    for (let j = 1; j < strs.length; j++) {
-      if (strs[j][i] !== letter) {
-        return prefix
-      }
-    }
-    prefix += letter
-  }
-  return prefix
+var isPalindrome = function (x) {
+  return Number(x.toString().split('').reverse().join('')) === x
 }
